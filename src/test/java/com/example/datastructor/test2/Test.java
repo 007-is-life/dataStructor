1ca5ee3a23550ca4bb6095f6b822f5bae928068e
ahead <1677306192@qq.com>
2022-02-13 13:19:41 +0800

    删除了测试源中的文件

--- src/test/java/com/example/datastructor/test2/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.datastructor.test2;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("afgsadf");
-    }
-}
--- src/test/java/com/example/datastructor/test2/Test2.java
+++
@@ -1,7 +0,0 @@
-package com.example.datastructor.test2;
-
-public class Test2 {
-    public static void main(String[] args) {
-        System.out.println("dev3分支");
-    }
-}
