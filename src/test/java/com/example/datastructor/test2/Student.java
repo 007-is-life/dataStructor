2c12e93f669b1d5012de3bb1857202c9e5be8e6a
ahead <1677306192@qq.com>
2022-02-13 14:08:58 +0800

    dev4_commit_2

--- src/test/java/com/example/datastructor/test2/Student.java
+++ src/test/java/com/example/datastructor/test2/Student.java
@@ -1,4 +1,8 @@
 package com.example.datastructor.test2;
 
 public class Student {
+    public static void main(String[] args) {
+        System.out.println("def");
+        System.out.println(123);
+    }
 }
